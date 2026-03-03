ea6c263262f15c37f53aae82e01bca726648e92e
Jose Martos <pepemartos98@gmail.com>
2026-03-03 13:11:07 +0100

    entrega ejercicio documentacion

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 8080;
-const { dbConnection } = require('./config/config');
-const routes = require('./routes');
+const PORT = process.env.PORT || 3000;
+const { dbConnection } = require("./config/config");
+const routes = require("./routes");
+
+const swaggerUI = require("swagger-ui-express");
+const docs = require("./docs/index");
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', routes);
-
+app.use("/", routes);
 
 dbConnection();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor activo en el puerto ${PORT}`));
+
+module.exports = app;
